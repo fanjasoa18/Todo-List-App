f749d2423e566ff777a8aef187f771d7f912bbbf
fanjasoa <fanjasoa18@gmail.com>
2023-02-07 20:58:50 +0300

    fix: update form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,7 @@
-import React , {useEffect} from "react";
+import React from "react";
 import {v4 as uuisv4} from "uuid";
 
-function Form({input, setInput, todos, setTodos, edit, setEdit}) {
-
-    const updateTodo = (title, id, completed) => {
-        const newTodo = todos.map((todo) => 
-            todo.id === id ? {title, id, completed} : todo
-        );
-        setTodos(newTodo);
-        setEdit("");
-    };
-
-    useEffect(() => {
-        if(edit) {
-            setInput(edit.title);
-        } else {
-            setInput("");
-        }
-    }, [setInput, edit]);
+function Form({input, setInput, todos, setTodos}) {
 
     const onInputChange = (event) => {
         setInput(event.target.value);
@@ -25,18 +9,15 @@ function Form({input, setInput, todos, setTodos, edit, setEdit}) {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        if(!edit) {
-            setTodos([...todos, {id: uuisv4(), title: input, completed: false}]);
-            setInput("");
-        }else {
-            updateTodo(input, edit.id,edit.completed)
-        }
-
+        setTodos([...todos, {id: uuisv4(), title: input, completed: false}]);
+        setInput("");
+        
     };
 
     return(
-       <form onSubmit={onFormSubmit} className="form-control">
+       <form data-testid="form" onSubmit={onFormSubmit} className="form-control">
             <input 
+                data-testid="form-input"
                 type="text" 
                 placeholder="Enter a Todo..." 
                 className="task-input"
@@ -44,11 +25,10 @@ function Form({input, setInput, todos, setTodos, edit, setEdit}) {
                 required
                 onChange={onInputChange}
             />
-            <button type="submit" className="btn-add">
-                {edit ? "OK" : "Add"}
-            </button>
+            
        </form> 
     )
 }
 
 export default Form;
+
